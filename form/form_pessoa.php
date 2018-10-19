d988f368b9598e69dbaea177b8a043022cb44cdf
ericksonmartinez <ericksonppu@gmail.com>
2018-10-19 18:08:31 -0400

    alguns ajuste layout

--- form/form_pessoa.php
+++ form/form_pessoa.php
@@ -3,13 +3,13 @@ include_once ("cabecalho.php");
 ?>
     <link rel="stylesheet" type="text/css" href="/css/form-pess.css">
     <div class="corpo">
-            <form calss="cadastro" action="/../public/adiciona_pessoa.php" method="POST">
+            <form calss="container cadastro" action="/../public/adiciona_pessoa.php" method="POST">
                      <legend class="titulo-cadastro">Novo Cadastro</legend>
                 <label for="tnome">Nome:</label>
-                        <input class="cnome" type="text" id="tnome" name="nome" required placeholder="Digite o nome"  pattern="[A-Za-z ']{3,}"> <br>
+                        <input class="cnome" type="text" id="tnome" name="nome" required placeholder="Digite o nome" pattern="[A-Za-z ']{3,}"> <br>
                 <label for="ttel">Telefone:</label>
-                        <input class="ctel" name="tel" type="tel" id="ttel" maxlength="11" placeholder="Digite o telefone"><br>
-                <label for="tcliente">Cliente</label>
+                        <input class="ctel" name="ttel" type="tel" id="itel" maxlength="11" placeholder="Digite o telefone"><br>
+                              <label for="tcliente">Cliente</label>
                         <input class="tipoacesso" id="tcliente" type="radio" name="tipoacesso" >
                 <label for="tusuario">Usuario</label>
                         <input class="tipoacesso" id="tusuario" type="radio" name="tipoacesso" >
